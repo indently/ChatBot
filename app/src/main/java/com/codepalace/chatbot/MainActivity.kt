40fbf3e09f37a94ea59e7733802c40382dc8308b
federicocotogno <federicosou@fastmail.se>
2020-09-24 12:24:49 +0200

    first step of the bot app

---
+++ app/src/main/java/com/codepalace/chatbot/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codepalace.chatbot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
